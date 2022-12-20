40f6e910bdd6e01b80334e69e9b3eff806881501
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2022-12-20 15:52:25 +0100

    Add some code to WarmupTwo

--- src/main/java/pl/mbrzozowski/map/Main.java
+++
@@ -1,8 +0,0 @@
-package pl.mbrzozowski.map;
-
-public class Main {
-
-    public static void main(String[] args) {
-        MapTwo mapTwo = new MapTwo();
-    }
-}
--- src/main/java/pl/mbrzozowski/warmup/WarmupTwo.java
+++ src/main/java/pl/mbrzozowski/warmup/WarmupTwo.java
@@ -1,25 +1,7 @@
 package pl.mbrzozowski.warmup;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class WarmupTwo {
 
-    protected final Logger logger = LoggerFactory.getLogger(getClass());
-
-    public void run() {
-        logger.info(stringTimes("Hi", 2));
-        logger.info(stringTimes("Hi", 3));
-        logger.info(stringTimes("Hi", 1));
-    }
-
-    /**
-     * Given a string and a non-negative int n, return a larger string that is n copies of the original string.
-     * <p>
-     * stringTimes("Hi", 2) → "HiHi"<p>
-     * stringTimes("Hi", 3) → "HiHiHi"<p>
-     * stringTimes("Hi", 1) → "Hi"<p>
-     */
     public String stringTimes(String str, int n) {
         String pattern = str;
         if (n == 0) {
@@ -29,6 +11,53 @@ public class WarmupTwo {
             str += pattern;
         }
         return str;
+    }
+
+    public String frontTimes(String str, int n) {
+        String substring = "";
+        StringBuilder result = new StringBuilder();
+        if (str.length() >= 3) {
+            substring = str.substring(0, 3);
+        } else {
+            substring = str;
+        }
+        for (int i = 0; i < n; i++) {
+            result.append(substring);
+        }
+        return result.toString();
+    }
+
+    int countXX(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            String substring = str.substring(i, i + 2);
+            if (substring.equals("xx")) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    boolean doubleX(String str) {
+        int indexSolo = str.indexOf('x');
+        int indexDouble = str.indexOf("xx");
+        return (indexSolo == indexDouble) && indexSolo != -1;
+    }
+
+    public String stringBits(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i += 2) {
+            result = result + str.substring(i, i + 1);
+        }
+        return result;
+    }
+
+    public String stringSplosion(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            result += str.substring(0, i + 1);
+        }
+        return result;
     }
 
 }
