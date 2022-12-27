f9a81ac9e7ba0dd509a04550f55731006184ffe1
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2022-12-27 15:12:09 +0100

    Add some tasks to LogicOne, StringTwo

---
+++ src/main/java/pl/mbrzozowski/logic/LogicOne.java
@@ -0,0 +1,123 @@
+package pl.mbrzozowski.logic;
+
+public class LogicOne {
+
+    public boolean cigarParty(int cigars, boolean isWeekend) {
+        if (isWeekend && cigars >= 40) {
+            return true;
+        } else {
+            return !isWeekend && cigars >= 40 && cigars <= 60;
+        }
+    }
+
+    public int dateFashion(int you, int date) {
+        if (you <= 2 || date <= 2) {
+            return 0;
+        } else if (you >= 8 || date >= 8) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+
+    public boolean squirrelPlay(int temp, boolean isSummer) {
+        if (!isSummer && temp >= 60 && temp <= 90) {
+            return true;
+        } else {
+            return isSummer && temp >= 60 && temp <= 100;
+        }
+    }
+
+    public int caughtSpeeding(int speed, boolean isBirthday) {
+        if (isBirthday) {
+            speed -= 5;
+        }
+        if (speed <= 60) {
+            return 0;
+        } else if (speed <= 80) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    public int sortaSum(int a, int b) {
+        int sum = a + b;
+        if (sum >= 10 && sum <= 19) {
+            return 20;
+        } else {
+            return sum;
+        }
+    }
+
+    public String alarmClock(int day, boolean vacation) {
+        if (!vacation) {
+            if (day >= 1 && day <= 5) {
+                return "7:00";
+            } else {
+                return "10:00";
+            }
+        } else {
+            if (day >= 1 && day <= 5) {
+                return "10:00";
+            } else {
+                return "off";
+            }
+        }
+    }
+
+    public boolean love6(int a, int b) {
+        return (a == 6 || b == 6) || (a + b == 6) || (Math.abs(a - b) == 6);
+    }
+
+    public boolean in1To10(int n, boolean outsideMode) {
+        if (outsideMode) {
+            return n <= 1 || n >= 10;
+        } else {
+            return n >= 1 && n <= 10;
+        }
+    }
+
+    public boolean specialEleven(int n) {
+        return n % 11 == 0 || n % 11 == 1;
+    }
+
+    public boolean more20(int n) {
+        return n % 20 == 1 || n % 20 == 2;
+    }
+
+    public boolean old35(int n) {
+        if ((n % 3 == 0) && (n % 5 != 0)) {
+            return true;
+        } else {
+            return (n % 3 != 0) && (n % 5 == 0);
+        }
+    }
+
+    public boolean less20(int n) {
+        return (n % 20 == 19) || (n % 20 == 18);
+    }
+
+    public boolean nearTen(int num) {
+        return num % 10 == 0 || num % 10 == 1 || num % 10 == 2 || num % 10 == 8 || num % 10 == 9;
+    }
+
+    public int teenSum(int a, int b) {
+        if ((a >= 13 && a <= 19) || (b >= 13 && b <= 19)) {
+            return 19;
+        } else {
+            return a + b;
+        }
+    }
+
+    public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+        if (isAsleep) {
+            return false;
+        } else if (isMorning && isMom) {
+            return true;
+        } else {
+            return !isMorning;
+        }
+    }
+
+}
---
+++ src/main/java/pl/mbrzozowski/string/StringTwo.java
@@ -0,0 +1,89 @@
+package pl.mbrzozowski.string;
+
+public class StringTwo {
+
+    public String doubleChar(String str) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            result.append(str.charAt(i)).append(str.charAt(i));
+        }
+        return result.toString();
+    }
+
+    public int countHi(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.startsWith("hi", i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean catDog(String str) {
+        int countCat = 0;
+        int countDog = 0;
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.startsWith("cat", i)) {
+                countCat++;
+            }
+            if (str.startsWith("dog", i)) {
+                countDog++;
+            }
+        }
+        return countCat == countDog;
+    }
+
+    public int countCode(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length() - 3; i++) {
+            if (str.startsWith("co", i) && str.substring(i, i + 4).endsWith("e")) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean endOther(String a, String b) {
+        return b.toLowerCase().endsWith(a.toLowerCase()) || a.toLowerCase().endsWith(b.toLowerCase());
+    }
+
+    public boolean xyzThere(String str) {
+        if (str.startsWith("xyz")) {
+            return true;
+        }
+        for (int i = 0; i < str.length() - 3; i++) {
+            if (str.charAt(i) != '.' && str.substring(i, i + 4).endsWith("xyz")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean bobThere(String str) {
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.startsWith("b", i) && str.substring(i, i + 3).endsWith("b")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean xyBalance(String str) {
+        return !str.contains("x") || (str.lastIndexOf("x") < str.lastIndexOf("y"));
+    }
+
+    public String mixString(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < Math.max(a.length(), b.length()); i++) {
+            if (i < a.length()) {
+                result.append(a.charAt(i));
+            }
+            if (i < b.length()) {
+                result.append(b.charAt(i));
+            }
+        }
+        return result.toString();
+    }
+
+}
