a1482e2cb67e64b6623f8843f69cd7eea06a3b53
Mateusz Brzozowski <mateusz_brzozowski93@wp.pl>
2023-01-10 14:33:01 +0100

    Finish tasks in String2

--- src/main/java/pl/mbrzozowski/string/String2.java
+++ src/main/java/pl/mbrzozowski/string/String2.java
@@ -86,4 +86,150 @@ public class String2 {
         return result.toString();
     }
 
+    public String repeatEnd(String str, int n) {
+        if (n == 0) {
+            return "";
+        }
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            result.append(str.substring(str.length() - n));
+        }
+        return result.toString();
+    }
+
+    public String repeatFront(String str, int n) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            result.append(str, 0, (n - i));
+        }
+        return result.toString();
+    }
+
+    public String repeatSeparator(String word, String sep, int count) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            result.append(word);
+            if (i != count - 1) {
+                result.append(sep);
+            }
+        }
+        return result.toString();
+    }
+
+    public boolean prefixAgain(String str, int n) {
+        char charAtIndex = str.charAt(n - 1);
+        for (int i = 0; i < str.length(); i++) {
+            if (i != n - 1 && str.charAt(i) == charAtIndex) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean xyzMiddle(String str) {
+        if (!str.contains("xyz")) {
+            return false;
+        }
+        String xyzMidOne = str.substring(str.length() / 2 - 1, str.length() / 2 + 2);
+        if (str.length() % 2 == 0) {
+            if (xyzMidOne.equals("xyz")
+                    || str.substring(str.length() / 2 - 2, str.length() / 2 + 1).equals("xyz")) {
+                return true;
+            }
+        }
+        return xyzMidOne.equals("xyz");
+    }
+
+    public String getSandwich(String str) {
+        final String bread = "bread";
+        if (str.length() <= bread.length() * 2) {
+            return "";
+        }
+        int firstIndexOf = str.indexOf(bread);
+        int lastIndexOf = str.lastIndexOf(bread);
+        if (firstIndexOf != lastIndexOf) {
+            return str.substring(firstIndexOf + 5, lastIndexOf);
+        }
+        return "";
+    }
+
+    public boolean sameStarChar(String str) {
+        for (int i = 1; i < str.length() - 1; i++) {
+            if (str.charAt(i) == '*' && (str.charAt(i - 1) != str.charAt(i + 1))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String oneTwo(String str) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length() - 2; i += 3) {
+            if (str.length() >= i + 3) {
+                result.append(str, i + 1, i + 3).append(str.charAt(i));
+            } else {
+                result.append(str.substring(i));
+            }
+        }
+        return result.toString();
+    }
+
+    public String zipZap(String str) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.length() >= i + 3 && str.charAt(i) == 'z' && str.charAt(i + 2) == 'p') {
+                result.append(str.charAt(i)).append(str.charAt(i + 2));
+                i += 2;
+            } else {
+                result.append(str.charAt(i));
+            }
+        }
+        return result.toString();
+    }
+
+    public String starOut(String str) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '*') {
+                i++;
+            } else if (i < str.length() - 1 && str.charAt(i + 1) == '*') {
+                i += 2;
+            } else {
+                if (i == 0 || str.charAt(i - 1) != '*') {
+                    result.append(str.charAt(i));
+                }
+            }
+        }
+        return result.toString();
+    }
+
+    public String plusOut(String str, String word) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.startsWith(word, i)) {
+                result.append(word);
+                i += word.length() - 1;
+            } else {
+                result.append("+");
+            }
+        }
+        return result.toString();
+    }
+
+    public String wordEnds(String str, String word) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i <= str.length() - word.length(); i++) {
+            if (str.startsWith(word, i)) {
+                if (i != 0) {
+                    result.append(str.charAt(i - 1));
+                }
+                if (i + word.length() < str.length()) {
+                    result.append(str.charAt(i + word.length()));
+                }
+            }
+        }
+        return result.toString();
+    }
+
+
 }
